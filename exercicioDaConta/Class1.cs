f3db96ee2d941f32d8b2733a05d8aa26cf923902
leonardomatumona <leonardomatumona@gmail.com>
2020-07-03 01:32:52 -0300

    Add project files.

---
+++ exercicioDaConta/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+    class Conta 
+    {
+        public string NomeTitular { get; set; }
+        public int  NumeroConta { get; set; }
+
+        public double Saldo { get; set; }
+
+        public double Depositar( double valor, double saldo)
+        {
+            if (saldo <= valor)
+            {
+                saldo += valor;
+            }
+            return saldo;
+        }
+
+        public double sacar(double valor , double saldo)
+        {
+            if (saldo <= valor)
+            {
+                saldo -= valor;
+            }
+            return saldo;
+        }
+
+       
+       
+
+
+
+
+    }
+}
---
+++ exercicioDaConta/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ContaBancaria;
+
+namespace exercicioDaConta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Conta1 = new Conta()
+            {
+               NomeTitular = "Luiz Castro Cavalcante",
+               NumeroConta = 8532,
+               Saldo = 500.0,
+            };
+
+            Console.WriteLine("Nome do Titular:" + Conta1.NomeTitular);
+            Console.WriteLine("Numero da Conta:" + Conta1.NumeroConta);
+            Console.WriteLine("Deposito Inicial:" + Conta1.Saldo);
+
+            Conta1.Depositar(500, Conta1.Saldo);
+
+            Console.WriteLine("Saldo Após Deposito:" + Conta1.Depositar(500, Conta1.Saldo));
+            
+
+            Conta1.sacar(200, Conta1.Saldo);
+
+            Console.WriteLine("Saldo Após Sacar:" + Conta1.sacar(200, Conta1.Saldo));
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
